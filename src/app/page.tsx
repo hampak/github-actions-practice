e71fb3c961dee08b6c280ec1534f563826eb6341
Haemin Park <haemin.parque@gmail.com>
2025-10-26 20:25:08 +0900

    testing stage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      UI Changes (Testing for real now)
+      Testing Stage
     </div>
   );
 }
