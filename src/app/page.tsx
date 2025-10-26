db999fe23f427bee90aa1ceca363348f6d03c601
Haemin Park <haemin.parque@gmail.com>
2025-10-26 20:38:10 +0900

    testing complete workflow (#10)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      Testing Stage
+      THis is a very good user feature that will be deployed to the stage
+      environment first.
     </div>
   );
 }
