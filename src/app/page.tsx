47e123b9732ff783b9db4d21f6baf67864a67af5
Haemin Park <haemin.parque@gmail.com>
2025-11-30 18:28:45 +0900

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       THis is a very good user feature that will be deployed to the stage
-      environment first. (ANOTHER ONE!!!) 12th
+      environment first. (ANOTHER ONE!!!) 12th!!!!!!!!!!!
     </div>
   );
 }
